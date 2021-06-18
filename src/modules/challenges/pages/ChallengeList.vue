42e4543fcb7f7640c41f48dce16b11997aea4aa3
jonathancierp <cierp.jonathan@gmail.com>
2021-06-18 14:21:02 +0200

    Feat : Challenge Page

--- src/modules/challenges/pages/ChallengeList.vue
+++ src/modules/challenges/pages/ChallengeList.vue
@@ -1,3 +1,33 @@
-<template>Test pour nicolas</template>
+<template>
+  <main class="container mx-auto my-10">
+    <h2 class="text-center text-2xl font-medium italic">Championnat Pépite</h2>
+    <div class="flex mt-10">
+      <table class="w-full">
+        <thead>
+          <tr class="border-b border-border">
+            <th class="bg-background-lighter py-5 px-5 text-left">Nom du challenge</th>
+            <th class="bg-background-lighter py-5 px-5 text-left">Statut</th>
+            <th class="bg-background-lighter py-5 px-5 text-left">Information</th>
+            <th class="bg-background-lighter py-5 px-5 text-left">Conseils</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-border">
+            <td class="bg-background-lighter py-3 px-5">Challenge 1</td>
+            <td class="bg-background-lighter py-3 px-5">
+              <span class="py-1 px-4 rounded-md font-medium text-sm bg-gray-100 text-gray-500">Statut 1</span>
+            </td>
+            <td class="bg-background-lighter py-3 px-5">Information 1</td>
+            <td class="bg-background-lighter py-3 px-5">
+              <p>Développeur PHP * 1</p>
+              <p>Développeur VueJS * 1</p>
+              <p>UI/UX Design * 2</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </main>
+</template>
 
 <script setup></script>
