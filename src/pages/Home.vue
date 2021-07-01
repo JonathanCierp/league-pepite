b58bb31ae98f59d9e77ddb06c463f8f03df09160
jonathancierp <cierp.jonathan@gmail.com>
2021-07-01 09:56:29 +0200

    Fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,3 @@
-<template>
-  <div class="container mx-auto mt-10 flex items-center justify-center h-96 border-4 border-border">
-    <img src="/logo-grafity.png" alt="" style="width: 150px" />
-    <img src="/logo-stratos.png" alt="" style="width: 150px" />
-  </div>
-  <div class="mt-6 container mx-auto text-center">
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-sm">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-lg">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl font-medium">
-      zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz
-    </p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl font-bold">
-      zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz
-    </p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-    <p class="text-2xl">zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeaz zeaeazzeaeazzeaeazzeaeazzeaeazzeaeaz</p>
-  </div>
-</template>
+<template>Home Page</template>
 
 <script></script>
