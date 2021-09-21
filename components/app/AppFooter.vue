91416948be0e5c079ca1342b9ff5f9ddf1bd4e42
Jonathan CIERP <cierp.jonathan@gmail.com>
2021-09-22 01:01:16 +0200

    Feat : footer bar

---
+++ components/app/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="flex items-center justify-between bg-background-lighter border-t border-border px-4 h-20">
+    <div></div>
+    <div class="flex">
+      <a class="border border-text rounded-full mr-8 p-3 cursor-pointer hover:text-orange-500 hover:border-orange-500" href="https://www.linkedin.com/company/league-pepite"
+         target="_blank">
+        <base-icon name="linkedin" size="22"/>
+      </a>
+      <a class="border border-text rounded-full mr-8 p-3 cursor-pointer hover:text-orange-500 hover:border-orange-500" href="https://twitter.com/PepiteLeague"
+         target="_blank">
+        <base-icon name="twitter" size="22"/>
+      </a>
+      <a class="border border-text rounded-full p-3 cursor-pointer hover:text-orange-500 hover:border-orange-500" href="https://www.facebook.com/profile.php?id=100072535893125"
+         target="_blank">
+        <base-icon name="facebook" size="22"/>
+      </a>
+    </div>
+    <div>
+      <a href="mailto:organisation@leaguepepite.com" class="font-medium uppercase text-sm hover:text-orange-500">Nous contacter</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,7 @@
   <div class="flex flex-col">
     <AppHeader />
     <nuxt />
-    <!--  <div class="h-screen flex flex-col">
-        <div class="flex-grow px-4 sm:px-8 md:px-12">
-          <HeaderBar />
-
-          <nuxt />
-        </div>
-
-        <Footer class="flex-none" />
-      </div>-->
+    <AppFooter />
   </div>
 </template>
 
