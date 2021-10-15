de6ca527921f129d40b374763d20099719cd7391
Jonathan CIERP <cierp.jonathan@gmail.com>
2021-10-15 23:51:08 +0200

    Feat : Add mentions legales

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="sm:sticky sm:bottom-0 grid grid-cols-1 sm:grid-cols-2 bg-background-lighter border-t border-border px-4 py-3 mt-auto sm:py-0">
-    <div class="flex items-center justify-center mb-2 sm:mb-0 sm:justify-start">
+  <footer class="sm:sticky sm:bottom-0 grid grid-cols-1 md:grid-cols-3 bg-background-lighter border-t border-border px-4 py-3 mt-auto md:py-0">
+    <div class="flex items-center justify-center mb-2 md:mb-0 md:justify-start">
       <a class="mr-4 p-3 cursor-pointer h-fit-content" href="https://www.linkedin.com/company/league-pepite"
          target="_blank">
         <base-icon name="linkedin" fill="#121c42" stroke="#121c42" stroke-width="1" size="20"/>
@@ -14,16 +14,32 @@
         <base-icon name="facebook" fill="#121c42" stroke="#121c42" stroke-width="1" size="20"/>
       </a>
     </div>
-    <div class="flex items-center justify-center sm:justify-end">
+    <div class="flex items-center justify-center md:justify-end md:col-span-2">
       <BaseLink
         to="/partners"
-        :class="[$route.path === '/partners' ? 'text-orange-500 border-b-2 border-orange-500' : '']"
-        class="flex items-center font-medium cursor-pointer uppercase text-sm py-4 md:py-6 lg:px-2 hover:text-orange-500"
+        :class="[$route.path === '/partners' ? 'text-orange-500' : '']"
+        class="flex items-center font-medium cursor-pointer uppercase text-sm py-4 md:py-6 md:px-2 hover:text-orange-500"
       >
         Nos partenaires
       </BaseLink>
       <span class="mx-4">|</span>
       <a href="mailto:organisation@leaguepepite.com" class="font-medium uppercase text-sm hover:text-orange-500">Nous contacter</a>
+      <span class="mx-4">|</span>
+      <BaseLink
+        to="/mentions-legales"
+        :class="[$route.path === '/mentions-legales' ? 'text-orange-500' : '']"
+        class="flex items-center font-medium cursor-pointer uppercase text-sm py-4 md:py-6 md:px-2 hover:text-orange-500"
+      >
+        Mentions légales
+      </BaseLink>
+<!--      <span class="mx-4">|</span>
+      <BaseLink
+        to="/cgu"
+        :class="[$route.path === '/cgu' ? 'text-orange-500' : '']"
+        class="flex items-center font-medium cursor-pointer uppercase text-sm py-4 md:py-6 md:px-2 hover:text-orange-500"
+      >
+        CGU
+      </BaseLink>-->
     </div>
   </footer>
 </template>
---
+++ pages/mentions-legales.vue
@@ -0,0 +1,77 @@
+<template>
+  <main v-if="isLoaded" class="container mx-auto my-4 lg:my-10 px-4 lg:px-0">
+    <h1 class="text-center uppercase text-5xl font-medium mb-28">Mentions Légales</h1>
+    <section class="mb-20">
+      <h2 class="text-left uppercase text-4xl mb-6">1. Informations légales</h2>
+      <p class="mb-4">Nous vous informons que:</p>
+      <p class="mb-4">La LEAGUE PEPITE est éditée par:</p>
+      <p class="mb-4">KATENGA SAS, une société de droit français dont le siège social est situé au 294 rue du cygne 45430 Mardie France. RCS n° 884487331.</p>
+      <p class="mb-4 flex items-center">
+        <base-icon name="check-square" class="text-orange-600 mr-2" />
+        Adresse de courrier électronique: organisation@leaguepepite.com
+      </p>
+      <p class="mb-4 flex items-center">
+        <base-icon name="check-square" class="text-orange-600 mr-2" />
+        Directeur de la publication: Paul MARTIN-DOUYAT
+      </p>
+      <p class="mb-4">L’hébergeur est : OVH – 2 rue Kellermann 59100 Roubaix 1007 https://www.ovh.com Téléphone: +33 (0)8 99 70 17 61.</p>
+    </section>
+    <section class="mb-20">
+      <h2 class="text-left uppercase text-4xl mb-6">2. Conditions d’utilisation</h2>
+      <p class="mb-4">Les informations fournies par KATENGA le sont à titre indicatif et ne sauraient dispenser l'utilisateur d'une analyse
+        complémentaire et personnalisée.</p>
+      <p class="mb-4">KATENGA ne saurait garantir l'exactitude, la complétude, l'actualité des informations diffusées sur son site internet
+        leaguepepite.com. En conséquence, l'utilisateur reconnaît utiliser ces informations sous sa responsabilité exclusive.</p>
+    </section>
+    <section class="mb-20">
+      <h2 class="text-left uppercase text-4xl mb-6">3. Protection des données</h2>
+      <p class="mb-4">L'utilisateur est notamment informé que conformément à la loi française de protection des données, les informations
+        qu'il communique par le biais des formulaires présents sur le site est nécessaire pour répondre à sa demande et sont destinées à KATENGA,
+        en tant que responsable du traitement à des fins de gestion administrative et commerciale.</p>
+      <p class="mb-4">L'utilisateur est informé qu'il dispose d'un droit d'accès, d'interrogation et de rectification qui lui
+        permet, le cas échéant, de faire rectifier, compléter, mettre à jour, verrouiller ou effacer les données personnelles
+        le concernant qui sont inexactes, incomplètes, équivoques, périmées ou dont la collecte, l'utilisation, la communication
+        ou la conservation est interdite.</p>
+      <p class="mb-4">L'utilisateur dispose également d'un droit d'opposition au traitement de ses données pour des motifs légitimes ainsi
+        qu'un droit d'opposition à ce que ces données soient utilisées à des fins de prospection commerciale.</p>
+      <p class="mb-4">L'ensemble de ces droits s'exerce auprès de KATENGA par courrier électronique accompagné d'une copie d'un titre d'identité en pièce jointe comportant une signature à adresser à :</p>
+      <a href="mailto:organisation@leaguepepite.com?subject=Droit d'accès aux données" class="mb-4 underline">organisation@leaguepepite.com</a>
+    </section>
+    <section class="mb-20">
+      <h2 class="text-left uppercase text-4xl mb-6">4. Cookies</h2>
+      <p class="mb-4">L'utilisateur est informé que lors de ses visites sur le site leaguepepite.com, un cookie peut s'installer automatiquement ayant pour but l'authentification de l'utilisateur.</p>
+      <p class="mb-4">Le cookie est un bloc de données qui ne permet pas d'identifier les utilisateurs mais sert à enregistrer des informations relatives à la navigation de celui-ci sur le site.</p>
+    </section>
+    <section class="mb-20">
+      <h2 class="text-left uppercase text-4xl mb-6">5. Propriété intellectuelle</h2>
+      <p class="mb-4">La structure générale ainsi que les logiciels, textes, images animées ou non, son savoir-faire et tous les autres éléments composant la LEAGUE PEPITE sont la propriété exclusive de KATENGA.</p>
+      <p class="mb-4">Toute représentation totale ou partielle de la LEAGUE PEPITE par quelque personne que ce soit, sans l'autorisation expresse de KATENGA est interdite et constituerait une contrefaçon sanctionnée par la loi française sur le copyright.</p>
+      <p class="mb-4">Les marques de LEAGUE PEPITE et de ses partenaires, ainsi que les logos figurant sur le site sont des marques déposées.</p>
+      <p class="mb-4">Toute reproduction totale ou partielle de ces marques ou de ces logos effectués à partir des éléments du site sans l'autorisation expresse de KATENGA est donc prohibée au sens du droit français.</p>
+      <p class="mb-4">KATENGA ne saurait être responsable de l'accès par les utilisateurs via les liens hypertextes mis en place dans le cadre du site internet LEAGUE PEPITE en direction d'autres ressources présentes sur le réseau.</p>
+      <p class="mb-4">Tout litige en relation avec l'utilisation du site est soumis au droit français. L'utilisateur reconnaît la compétence exclusive des tribunaux français compétents.</p>
+    </section>
+  </main>
+</template>
+
+<script>
+import { ref, onMounted } from '@nuxtjs/composition-api'
+
+export default {
+  layout: 'default',
+  head: {
+    title: 'Mentions Légales | League PEPITE'
+  },
+  setup() {
+    const isLoaded = ref(false)
+
+    onMounted(async () => {
+      isLoaded.value = true
+    })
+
+    return {
+      isLoaded
+    }
+  }
+}
+</script>
