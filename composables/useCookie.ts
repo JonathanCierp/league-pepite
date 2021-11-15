50fceaecd8265d180062fb89e7fb30583905a8f6
Jonathan <jcierp@katenga.fr>
2021-11-15 17:57:48 +0100

    Fix : cookie

--- composables/useCookie.ts
+++ composables/useCookie.ts
@@ -7,8 +7,7 @@ class Cookie {
   }
   
   addCookie = (key: string, value: string, domain: string) => {
-    console.log(`${key}=${value}; domain=${domain}; path=/;`)
-    document.cookie = `${key}=${value}; domain=${domain}; path=/;`
+    document.cookie = `${key}=${value};`
   }
 
   deleteCookie = (key: string) => {
